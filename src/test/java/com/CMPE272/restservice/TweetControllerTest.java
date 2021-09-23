1425e0254e8c3e54cc3c23fb9cac0fc4fa19bef8
Sarinder Virk <sarinder.virk@oracle.com>
2021-09-22 19:27:34 -0700

    more unit tests

--- src/test/java/com/CMPE272/restservice/TweetControllerTest.java
+++ src/test/java/com/CMPE272/restservice/TweetControllerTest.java
@@ -1,8 +1,11 @@
 package com.CMPE272.restservice;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,12 +16,37 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+/**
+ * Created by Sarinder Virk
+ */
 @SpringBootTest
 @AutoConfigureMockMvc
 public class TweetControllerTest {
 
 	@Autowired
 	private MockMvc mvc;
+
+	// in order to avoid creating numerous test tweets, keep track of the 
+	// created tweet id so it can then be deleted
+	//private String createdId;
+
+	String bad_id = "1110458664540659720";
+	String good_id = "1440458664540659720";
+
+	@Test
+	public String postTweet() throws Exception {
+		String request = "unit test " + new Date();
+		String response = mvc.perform(MockMvcRequestBuilders
+			.post("/tweet?text=" + request)
+			.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+		System.out.println("response="+ response);
+		// response should contain json with substring like ..."id":"1440857187002171399"...
+		Matcher m = Pattern.compile("\"id\":\"(\\d+)\"").matcher(response);
+		String createdId = m.find() ? m.group(1) : null;
+		assertNotNull(createdId);
+		return createdId;
+	}
 
 	@Test
 	public void readTweetMissingId() throws Exception {
@@ -28,18 +56,37 @@ public class TweetControllerTest {
 
 	@Test
 	public void readTweetIdNotFound() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get("/tweet?id=1110458664540659720").accept(MediaType.APPLICATION_JSON))
+		mvc.perform(MockMvcRequestBuilders.get("/tweet?id=" + bad_id).accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isNotFound());
 	}
 
 	@Test
 	public void readTweet() throws Exception {
-		String id = "1440458664540659720";
-		mvc.perform(MockMvcRequestBuilders.get("/tweet?id="+ id).accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andReturn()
-				.getResponse()
-				.getContentAsString()
-				.contains("{\"id\":\"" + id + "\"}");
+		mvc.perform(MockMvcRequestBuilders.get("/tweet?id=" + good_id).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk()).andReturn().getResponse().getContentAsString()
+				.contains("{\"id\":\"" + good_id + "\"}");
 	}
+
+	@Test
+	public void deleteTweet() throws Exception {
+		String createdId = postTweet();
+		System.out.println("createdId="+ createdId);
+		mvc.perform(MockMvcRequestBuilders.delete("/tweet?id=" + createdId).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void deleteTweetNotFound() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.delete("/tweet?id="+ bad_id).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNotFound());
+	}
+
+	@Test
+	public void listTimelineTest() throws Exception {
+		String good_id = "1440458664540659720";
+		mvc.perform(MockMvcRequestBuilders.get("/timeline").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk()).andReturn().getResponse().getContentAsString()
+				.contains("{\"id\":\"" + good_id + "\"}");
+	}
+
 }
